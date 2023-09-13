7ae358c880b1d6be854cebde706eb5fa61d420c8
Stefan <stefan.frank.sund@gmail.com>
2023-09-13 23:25:55 +0200

    La till ett repo

---
+++ src/main/java/com/example/tefanstrevligatroligatest/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.tefanstrevligatroligatest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+        Optional<User> findUserByUsername(String username);
+}
