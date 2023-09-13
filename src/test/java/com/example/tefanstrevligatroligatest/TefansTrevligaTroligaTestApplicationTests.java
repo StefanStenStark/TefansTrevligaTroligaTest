b477c85c30b39d22faa3c557a75259288d48ce57
Stefan <stefan.frank.sund@gmail.com>
2023-09-13 22:57:55 +0200

    can_get_username_with_getter Röd

--- src/test/java/com/example/tefanstrevligatroligatest/TefansTrevligaTroligaTestApplicationTests.java
+++ src/test/java/com/example/tefanstrevligatroligatest/TefansTrevligaTroligaTestApplicationTests.java
@@ -3,11 +3,25 @@ package com.example.tefanstrevligatroligatest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+
 @SpringBootTest
 class TefansTrevligaTroligaTestApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    void can_get_username_with_getter() {
+        //Given
+        String username = "Tefis";
+        String password = "TefisIsBestis";
+        User user = new User(username, password);
+
+        //When
+        String getUsername = user.getUsername();
+
+        //Then
+        assertEquals(getUsername, username);
+
     }
 
 }
