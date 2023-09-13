e41c9a81cc8472297324849feefe881154f5016c
Stefan <stefan.frank.sund@gmail.com>
2023-09-13 22:43:52 +0200

    Initial commit

---
+++ src/main/java/com/example/tefanstrevligatroligatest/TefansTrevligaTroligaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tefanstrevligatroligatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TefansTrevligaTroligaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TefansTrevligaTroligaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tefanstrevligatroligatest/TefansTrevligaTroligaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tefanstrevligatroligatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TefansTrevligaTroligaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
