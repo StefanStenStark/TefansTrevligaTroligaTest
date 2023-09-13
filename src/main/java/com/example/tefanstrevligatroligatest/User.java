16acb2637a3061732380578e7750c7f5c334e289
Stefan <stefan.frank.sund@gmail.com>
2023-09-13 23:02:39 +0200

    can_get_username_with_getter Gul fas

---
+++ src/main/java/com/example/tefanstrevligatroligatest/User.java
@@ -0,0 +1,20 @@
+package com.example.tefanstrevligatroligatest;
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return "WokaWokaEeOoh";
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
